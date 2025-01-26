57c3ee27f0a6d103ea243c7d06a0eaeab3cea5ea
Rit Narayan Mallick <120083816+74892Rit@users.noreply.github.com>
2025-01-27 04:28:09 +0530

    imported react

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable camelcase */
-
+import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Inter, Space_Grotesk } from "next/font/google";
